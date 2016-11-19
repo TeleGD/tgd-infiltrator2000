bfc764d996a30cb2f3cce180adb7ee24db7433a6
Valentin Moreau <Valentin Moreau>
2016-11-19 16:27:15 +0100

    modif render et update guard

--- src/entity/movable/Circle/Guard.java
+++ src/entity/movable/Circle/Guard.java
@@ -1,8 +1,8 @@
 package entity.movable.Circle;
 
+import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
-import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class Guard extends Character {
@@ -26,11 +26,11 @@ public class Guard extends Character {
 		this.visualField = visualField;
 	}
 	
-	public void update(BasicGameState arg1, StateBasedGame arg2, int arg3)  {
+	public void update(GameContainer arg1, StateBasedGame arg2, int arg3)  {
 		
 	}
 	
-	public void render(BasicGameState arg1, StateBasedGame arg2, Graphics arg3){
+	public void render(GameContainer arg1, StateBasedGame arg2, Graphics arg3){
 		arg3.fillOval((float)x, (float)y, (float)2*radius, (float)2*radius);
 	}
 	
@@ -47,24 +47,6 @@ public class Guard extends Character {
 			}
 			bool=1;
 		}
-	}
-
-	@Override
-	public int getRadius() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int getPositionX() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int getPositionY() {
-		// TODO Auto-generated method stub
-		return 0;
 	}
 
 	
