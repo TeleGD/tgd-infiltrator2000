4429545f02683f5344ae4ae78c4eca344faf3293
Valentin Moreau <Valentin Moreau>
2016-11-19 16:08:09 +0100

    modif guarde

--- src/entity/movable/Guard.java
+++ src/entity/movable/Guard.java
@@ -1,9 +1,12 @@
 package entity.movable;
 
+import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
 
 public class Guard extends Character {
-
+	private double radius; 
 	protected Area visualField;
 	
 	public Guard(double x, double y, double sx, double sy, Image im, Area visuFie) {
@@ -21,6 +24,29 @@ public class Guard extends Character {
 	
 	public void setVisualField(Area visualField) {
 		this.visualField = visualField;
+	}
+	
+	public void update(BasicGameState arg1, StateBasedGame arg2, int arg3)  {
+		
+	}
+	
+	public void render(BasicGameState arg1, StateBasedGame arg2, Graphics arg3){
+		arg3.fillOval((float)x, (float)y, (float)2*radius, (float)2*radius);
+	}
+	
+	public void movX(double x, double sx, double count, double bool, int delta){
+		if (bool == 1){
+			while (x<count){
+				moveX(delta);
+			}
+			bool=-1;
+		}
+		else{
+			while (x>-count){
+				moveX(delta);
+			}
+			bool=1;
+		}
 	}
 
 	
