d0d4e1d0a05b36a46cf671088328b93e04faf4e2
canalarthur <canalarthur@yahoo.fr>
2016-11-19 23:15:42 +0100

    STEP INSIDE

--- src/fr/main/Game.java
+++ src/fr/main/Game.java
@@ -1,6 +1,5 @@
 package fr.main;
 
-
 import java.io.File;
 
 import org.newdawn.slick.AppGameContainer;
@@ -8,23 +7,18 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
+import fr.basic.menu.*;
 import fr.basic.World;
 
-
-
-
 public class Game extends StateBasedGame {
-
 	
 	public Game(String name) {
 		super(name);
 	}
 
 	public Game() {
-		super("pouletz0r");
+		super("|NF|1TR470R 2000");
 	}
-
-
 
 	public static void main(String[] args) throws SlickException {
 		AppGameContainer app = new AppGameContainer(new Game(),1280,720,false);
@@ -33,17 +27,11 @@ public class Game extends StateBasedGame {
 		app.setShowFPS(true);
 		app.start();
 	}
-
-
-
-
+	
 	@Override
 	public void initStatesList(GameContainer container) throws SlickException {
-//		addState(new Mainmenu());
+ 		addState(new MenuPrincipal());
 		addState(new World());
-//		addState(new MenuSortie());
-//		addState(new MenuFinPartie());
 	}
-
 
 }
