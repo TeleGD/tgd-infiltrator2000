37818e97b2c35f218364845e2567a7e2c7e018d7
Blablos <plop@gmail.com>
2016-11-19 19:00:26 +0100

    MovableRectangle rempli

--- src/entity/movable/rectangle/MovableRectangle.java
+++ src/entity/movable/rectangle/MovableRectangle.java
@@ -7,33 +7,75 @@ import fr.interfaces.Rectangle;
 
 public class MovableRectangle extends Movable implements Rectangle{
 
-	public MovableRectangle(double x, double y, double sx, double sy) {
+	protected double height;
+	protected double width;
+	
+	public MovableRectangle(double x, double y, double sx, double sy, double height, double width) {
 		super(x, y, sx, sy);
-		// TODO Auto-generated constructor stub
+		this.height=height;
+		this.width=width;
 	}
 
 	@Override
 	public double getHeight() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.height;
 	}
 
 	@Override
 	public double getWidth() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.width;
 	}
 
 	@Override
 	public ArrayList<ArrayList<Double>> pixelBordeer() {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<ArrayList<Double>> res=new ArrayList<ArrayList<Double>>();
+		for (int i = (int) this.x; i < this.x+this.width ; i++) {
+			ArrayList<Double> a=new ArrayList<Double>();
+			a.add((double)i);
+			a.add(this.y);
+			res.add(a);
+			ArrayList<Double> b=new ArrayList<Double>();
+			b.add((double)i);
+			b.add(this.y+this.height);
+			res.add(b);
+		}
+		for (int j = (int) this.y; j <this.y+this.height ; j++) {
+			ArrayList<Double> a=new ArrayList<Double>();
+			a.add(this.x);
+			a.add((double) j);
+			res.add(a);
+			ArrayList<Double> b=new ArrayList<Double>();
+			b.add(this.x+this.width);
+			b.add((double) j);
+			res.add(b);
+		}
+		return res;
 	}
 
 	@Override
 	public ArrayList<ArrayList<Double>> pixelBordeerWithSpeed() {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<ArrayList<Double>> res=new ArrayList<ArrayList<Double>>();
+		for (int i =(int) ((int) this.x+this.getSpeedX()); i < this.x+this.width+this.getSpeedX() ; i++) {
+			ArrayList<Double> a=new ArrayList<Double>();
+			a.add((double)i);
+			a.add(this.y+this.getSpeedY());
+			res.add(a);
+			ArrayList<Double> b=new ArrayList<Double>();
+			b.add((double)i);
+			b.add(this.y+this.height+this.getSpeedY());
+			res.add(b);
+		}
+		for (int j = (int) ((int) this.y+this.getSpeedY()); j <this.y+this.height+this.getSpeedY() ; j++) {
+			ArrayList<Double> a=new ArrayList<Double>();
+			a.add(this.x+this.getSpeedX());
+			a.add((double) j);
+			res.add(a);
+			ArrayList<Double> b=new ArrayList<Double>();
+			b.add(this.x+this.width+this.getSpeedX());
+			b.add((double) j);
+			res.add(b);
+		}
+		return res;
 	}
 
 }
