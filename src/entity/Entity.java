f5888075a037fb6b9dedc3abf66c6ff6bc075b04
Valentin Moreau <Valentin Moreau>
2016-11-19 16:55:04 +0100

    modif render et update

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -2,6 +2,7 @@ package entity;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
@@ -36,11 +37,11 @@ public abstract class Entity {
 		this.y = y;
 	}
 	
-	public void update(GameContainer arg1, StateBasedGame arg2, int arg3)  {
+	public void update(GameContainer arg1, StateBasedGame arg2, int arg3) throws SlickException  {
 		
 	}
 	
-	public void render( GameContainer arg1, StateBasedGame arg2, Graphics arg3){
+	public void render( GameContainer arg1, StateBasedGame arg2, Graphics arg3) throws SlickException{
 	
 	}
 
--- src/entity/movable/Circle/Guard.java
+++ src/entity/movable/Circle/Guard.java
@@ -3,16 +3,19 @@ package entity.movable.Circle;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
+
+import fr.vision.Area;
 
 public class Guard extends Character {
 	private double radius; 
 	protected Area visualField;
 	
-	public Guard(double x, double y, double sx, double sy, Image im, Area visuFie) {
+	public Guard(double x, double y, double sx, double sy, /*Image im,*/ Area visuFie) {
 		super(x, y, sx, sy, im);
-		this.visualField=visuFie;
+		//this.visualField=visuFie;
 	}
 
 	//------------------------------Get--------------
@@ -27,11 +30,11 @@ public class Guard extends Character {
 		this.visualField = visualField;
 	}
 	
-	public void update(GameContainer arg1, StateBasedGame arg2, int arg3)  {
+	public void update(GameContainer arg1, StateBasedGame arg2, int arg3) throws SlickException  {
 		
 	}
 	
-	public void render(GameContainer arg1, StateBasedGame arg2, Graphics arg3){
+	public void render(GameContainer arg1, StateBasedGame arg2, Graphics arg3) throws SlickException {
 		arg3.fillOval((float)x, (float)y, (float)(2*radius), (float)(2*radius));
 	}
 	
